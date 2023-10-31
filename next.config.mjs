a284afa1d8aee8b00bc92eacd4541296719ff389
adon94 <adambfad@gmail.com>
2023-10-31 16:26:14 +0000

    reduce serverless dependencies size

--- next.config.mjs
+++ next.config.mjs
@@ -12,10 +12,9 @@ const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
   experimental: {
     outputFileTracingExcludes: {
       "*": [
-        "node_modules/@swc/**/*",
-        "node_modules/@esbuild/**/*",
-        "node_modules/terser/**/*",
-        "node_modules/webpack/**/*",
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
       ],
     },
   },
