f551d4d7746b8f509bb396d5424721061288c00b
adon94 <adambfad@gmail.com>
2023-10-31 15:23:42 +0000

    add login and registration

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,33 +1,38 @@
+import { sql } from "@vercel/postgres";
+import bcrypt from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
 
 interface CustomBody {
-  id: string;
   email: string;
   password: string;
   name?: string;
 }
 
-// acts as a mock api
 export async function POST(request: NextRequest) {
   const user = (await request.json()) as CustomBody;
 
-  if (user) {
-    return NextResponse.json(
-      {
-        message: "User registered successfully.",
-      },
-      {
-        status: 201,
-      }
-    );
+  // Validation
+  if (!user.email || !user.password) {
+    return NextResponse.json({ error: "Email and password are required." }, { status: 400 });
+  }
+
+  // Check for existing users with the same email
+  const { rows: existingUsers } = await sql`SELECT * FROM users WHERE email = ${user.email}`;
+
+  if (existingUsers.length > 0) {
+    return NextResponse.json({ error: "A user with the same email already exists!" }, { status: 409 });
+  }
+
+  // Hashing the password
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+
+  // Insert the new user into the database
+  const { rows: newUsers } =
+    await sql`INSERT INTO users (email, hashed_password, name) VALUES (${user.email}, ${hashedPassword}, ${user.name}) RETURNING *`;
+
+  if (newUsers.length > 0) {
+    return NextResponse.json({ message: "User registered successfully." }, { status: 201 });
   } else {
-    return NextResponse.json(
-      {
-        error: "A user with the same email already exists!",
-      },
-      {
-        status: 405,
-      }
-    );
+    return NextResponse.json({ error: "Registration failed. Please try again." }, { status: 500 });
   }
 }
