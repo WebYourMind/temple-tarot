56b074de8f176e9aa7d2971bb8d7ff974d644e29
adon94 <adambfad@gmail.com>
2023-11-16 17:07:35 +0000

    fix report gen

--- app/api/report/route.ts
+++ app/api/report/route.ts
@@ -4,6 +4,8 @@ import { Score } from "lib/quiz";
 import { sql } from "@vercel/postgres";
 import { getSession } from "lib/auth";
 
+export const runtime = "edge";
+
 const createReportGenerationPrompt = ({
   explorer,
   analyst,
@@ -25,7 +27,7 @@ const createReportGenerationPrompt = ({
 Generate a comprehensive insight report titled within the context of thinking styles and 'Shift Thinking': 'Your Thinking Style Results' in markdown format for a user primarily identified as a "${dominantStyle}". Explicitly state their dominant thinking style the beginning of the report. The user's thinking style profile is as follows: explorer(${explorer}), analyst(${analyst}), designer(${designer}), optimizer(${optimizer}), connector(${connector}), nurturer(${nurturer}), energizer(${energizer}), achiever(${achiever}). Without explicitly stating it, align the report on the teachings of Mark Bonchek and shift.to methodology. The report should:
 
 1. Focus primarily on the "${dominantStyle}" thinking archetype, offering detailed strategies for personal growth, learning, decision-making, problem-solving, and maintaining motivation.
-2. Include insights and personalized advice for the 3 highest scored thinking styles only, ensuring a comprehensive understanding of their multifaceted thinking approach.
+2. Include insights and personalized advice for the highest scored thinking styles, ensuring a comprehensive understanding of the user's multifaceted thinking approach.
 3. Provide recommendations for enhancing interpersonal relationships, considering their communicative and caring scores.
 4. Offer ideas for managing change and uncertainty in both personal and professional contexts.
 5. Suggest techniques for maintaining energy and motivation, specifically tailored to activities that best suit their dominant thinking archetype.
