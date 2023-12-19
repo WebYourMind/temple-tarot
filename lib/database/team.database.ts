53bdfd8ea4ee0670ad98726d75963fb53bca056b
Hamza Naseer <hamza.naseer@purelogics.com>
2023-12-19 23:20:11 +0500

    US 4.5 BE: In progress Done GET: get Team Score

--- lib/database/team.database.ts
+++ lib/database/team.database.ts
@@ -1,23 +1,81 @@
-
 import { sql } from "@vercel/postgres";
 
-
-export const verifyTeamToken = async (token: string) =>  {
-    try {
-        const {rows} = await sql`
+export const verifyTeamToken = async (token: string) => {
+  try {
+    const { rows } = await sql`
         SELECT * FROM teams WHERE invite_token = ${token}
     `;
 
-        if(rows.length > 0) {
-            const team = rows[0];
-            if(team.invite_token_expiry > new Date()){
-                return team;
-            }
-        }
-        return null;
+    if (rows.length > 0) {
+      const team = rows[0];
+      if (team.invite_token_expiry > new Date()) {
+        return team;
+      }
     }
-    catch (error) {
-        console.error(error);
-        return null;
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getTeamById = async (teamId: number) => {
+  try {
+    const { rows } = await sql`
+        SELECT * FROM teams WHERE id = ${teamId}
+    `;
+
+    if (rows.length > 0) {
+      return rows[0];
     }
-}
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getTeamScore = async (teamId: number) => {
+  try {
+    const { rows } = await sql`            
+              SELECT t.id as team_id,
+                  t.name as team_name,
+                  t.description as team_description,
+                  t.admin_id as team_admin_id,
+                  t.image as team_image,
+                  u.id as user_id,
+                  u.name as user_name,
+                  u.email as user_email,
+                  u.phone as user_phone,
+                  u.role as user_role,
+                  s.id as score_id,
+                  s.explorer as explorer,
+                  s.analyst as analyst,
+                  s.designer as designer,
+                  s.optimizer as optimizer,
+                  s.connector as connector,
+                  s.nurturer as nurturer,
+                  s.energizer as energizer,
+                  s.achiever as achiever
+              FROM users u
+              LEFT JOIN scores s ON (
+              s.user_id = u.id
+                 AND NOT EXISTS (
+                   SELECT 1 FROM scores s1
+                   WHERE s1.id = s.id
+                     AND s1.id > u.id
+                 )
+              )
+              join teams t on t.id = u.team_id
+              WHERE u.team_id = 5;
+        `;
+
+    if (rows.length > 0) {
+      return rows;
+    }
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
--- lib/database/user.database.ts
+++ lib/database/user.database.ts
@@ -1,14 +1,13 @@
 import { sql } from "@vercel/postgres";
 
 export const updateUserTeam = async (userId: number, teamId: number) => {
-    try {
-        const { rows } = await sql`
+  try {
+    const { rows } = await sql`
         UPDATE users SET team_id = ${teamId} WHERE id = ${userId}
       `;
-        return rows[0];
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
-
-}
+    return rows[0];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
