735fa99b1c4d5cb55c95585a7b2232776ad0edd6
Hamza Naseer <hamza.naseer@purelogics.com>
2023-12-19 21:32:35 +0500

    US 4.4 BE: API for verify team token API for update user's team_id

---
+++ lib/database/user.database.ts
@@ -0,0 +1,14 @@
+import { sql } from "@vercel/postgres";
+
+export const updateUserTeam = async (userId: number, teamId: number) => {
+    try {
+        const { rows } = await sql`
+        UPDATE users SET team_id = ${teamId} WHERE id = ${userId}
+      `;
+        return rows[0];
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+
+}
