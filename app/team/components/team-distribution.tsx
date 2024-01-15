9bb561cfeb418273c5dedecc80b99871d56ffd03
adon94 <adambfad@gmail.com>
2024-01-15 14:33:06 +0000

    add radar chart

--- app/team/components/team-distribution.tsx
+++ app/team/components/team-distribution.tsx
@@ -1,8 +1,23 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import appConfig from "app.config";
 import Card from "components/card";
+import { Button } from "components/ui/button";
 import { thinkingStyleDescriptions } from "lib/ArchetypePieChart";
 import { ThinkingStyle, UserProfile } from "lib/types";
-import React from "react";
-import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import React, { useState } from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar,
+} from "recharts";
 
 const COLORS = [
   "#0088FE", // Blue
@@ -38,6 +53,7 @@ export const CustomTooltip = ({ active, payload }: any) => {
 };
 
 const ThinkingStyleDistribution = ({ teamMembers }: Props) => {
+  const [currentPage, setCurrentPage] = useState(0);
   // Calculate the distribution of thinking styles
   const styleCounts = teamMembers.reduce((acc: any, member: UserProfile) => {
     const styleKey = member?.dominantStyle ? member.dominantStyle : "Assessment Pending";
@@ -46,32 +62,60 @@ const ThinkingStyleDistribution = ({ teamMembers }: Props) => {
   }, {});
 
   const data = Object.keys(styleCounts).map((key) => ({ name: key, value: styleCounts[key] }));
+  const radarData = data.map((d) => ({ ...d, value: (d.value / data.length) * 100 }));
 
   return (
     <Card>
-      <div className="flex flex-col items-center">
+      <div className="flex h-[420px] flex-col items-center justify-between">
         <h2 className="mb-4 text-center text-2xl font-bold">Team Distribution</h2>
-        <p>(Hover for more info)</p>
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart>
-            <Pie
-              data={data}
-              cx="50%"
-              cy="50%"
-              labelLine={false}
-              label={({ name, percent }) => `${name} (${(percent * 100).toFixed(0)}%)`}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-            <Tooltip content={<CustomTooltip />} />
-            <Legend />
-          </PieChart>
-        </ResponsiveContainer>
+        {currentPage === 0 ? (
+          <ResponsiveContainer width="100%" height={300}>
+            <RadarChart cx="50%" cy="50%" outerRadius="80%" data={radarData}>
+              <PolarGrid />
+              <PolarAngleAxis dataKey="name" />
+              <PolarRadiusAxis tickFormatter={(tick) => tick + "%"} />
+              <Radar
+                name="Team"
+                dataKey="value"
+                stroke={appConfig.theme.modes.light.primary}
+                fill={appConfig.theme.modes.light.primary}
+                fillOpacity={0.6}
+              />
+            </RadarChart>
+          </ResponsiveContainer>
+        ) : (
+          <>
+            <p className="text-muted">(Hover for more info)</p>
+            <ResponsiveContainer width="100%" height={300}>
+              <PieChart>
+                <Pie
+                  data={data}
+                  cx="50%"
+                  cy="50%"
+                  labelLine={false}
+                  label={({ name, percent }) => `${name} (${(percent * 100).toFixed(0)}%)`}
+                  outerRadius={80}
+                  fill="#8884d8"
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+                <Tooltip content={<CustomTooltip />} />
+                <Legend />
+              </PieChart>
+            </ResponsiveContainer>
+          </>
+        )}
+        <div className="flex w-full justify-between">
+          <Button variant={"outline"} disabled={currentPage <= 0} onClick={() => setCurrentPage(currentPage - 1)}>
+            <ArrowLeftIcon />
+          </Button>
+          <Button variant={"outline"} disabled={currentPage >= 1} onClick={() => setCurrentPage(currentPage + 1)}>
+            <ArrowRightIcon />
+          </Button>
+        </div>
       </div>
     </Card>
   );
