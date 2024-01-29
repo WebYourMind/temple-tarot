dd47f3bcd7c01d6c1145cdacb937f14f85054341
adon94 <adambfad@gmail.com>
2024-01-29 14:49:28 +0000

    update team layout

--- app/team/components/team.tsx
+++ app/team/components/team.tsx
@@ -198,7 +198,6 @@ export default function Team() {
           </Card>
 
           {team.users && <ThinkingStyleDistribution teamMembers={team.users} />}
-          <InviteLink team={team} inviterName={session?.user?.name} />
         </div>
 
         <div className="space-y-4 md:col-span-1">
@@ -210,6 +209,9 @@ export default function Team() {
               adminId={team.adminId}
             />
           )}
+        </div>
+        <div className="md:col-span-2">
+          <InviteLink team={team} inviterName={session?.user?.name} />
         </div>
         <div className="md:col-span-2">
           <Card>
