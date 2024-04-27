91fde8c1daa85220eebc7ac8a90925868296ebf2
adon94 <adambfad@gmail.com>
2024-04-27 12:44:17 +0100

    change spread selector to dialog

--- app/(views)/home/components/spread-selector.tsx
+++ app/(views)/home/components/spread-selector.tsx
@@ -1,15 +1,8 @@
 import * as React from "react";
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "components/ui/select";
 import tarotSpreads from "./tarot-spreads";
+import { DialogContent, DialogTrigger } from "components/ui/dialog";
+import { ChevronDown } from "lucide-react";
 
 export function SpreadSelector({ onSpreadSelect, selectedSpread }) {
   const handleSpreadSelect = (spreadValue) => {
@@ -17,26 +10,29 @@ export function SpreadSelector({ onSpreadSelect, selectedSpread }) {
     onSpreadSelect(selectedSpread);
   };
   return (
-    <Select onValueChange={handleSpreadSelect} value={selectedSpread?.value || "single_card"}>
-      <SelectTrigger className="truncate-text mb-2 w-72">
-        <SelectValue placeholder="Choose a spread" />
-      </SelectTrigger>
-      <SelectContent className="fixed inset-x-0 top-0 z-50 max-h-[30vh] w-72 overflow-y-auto shadow-lg md:max-h-[40vh]">
-        <SelectGroup>
-          <SelectLabel className="px-4 pb-2 pt-4 text-sm font-semibold ">Spreads</SelectLabel>
-          {tarotSpreads.map((spread) => (
-            <SelectItem key={spread.value} value={spread.value} className="cursor-pointer">
-              <div className="flex flex-col">
-                <p className="my-0 text-sm font-semibold">{spread.name}</p>
-                <p className="mb-1 text-xs font-bold opacity-80">
-                  {spread.numberOfCards} card{spread.numberOfCards > 1 ? "s" : ""}
-                </p>
-                <p className="mt-0 text-xs opacity-80">{spread.description}</p>
-              </div>
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <>
+      <DialogTrigger className="flex space-x-2">
+        <span>{selectedSpread?.name}</span> <ChevronDown />
+      </DialogTrigger>
+      <DialogContent className="my-16 max-h-[80vh] max-w-[90vw] overflow-scroll rounded-md ">
+        <h2>Spreads</h2>
+        {tarotSpreads.map((spread) => (
+          <button
+            key={spread.value}
+            value={spread.value}
+            onClick={() => handleSpreadSelect(spread.value)}
+            className="w-full cursor-pointer rounded py-6 transition hover:bg-accent md:px-4"
+          >
+            <div className="flex flex-col justify-start text-left">
+              <p className="my-0 text-lg font-semibold">{spread.name}</p>
+              <p className="mb-1 font-bold opacity-80">
+                {spread.numberOfCards} card{spread.numberOfCards > 1 ? "s" : ""}
+              </p>
+              <p className="mt-0 opacity-80">{spread.description}</p>
+            </div>
+          </button>
+        ))}
+      </DialogContent>
+    </>
   );
 }
