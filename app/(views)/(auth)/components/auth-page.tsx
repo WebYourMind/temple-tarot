463387813fa1a56f15824c6cc21c8aac767685fa
adon94 <adambfad@gmail.com>
2024-04-27 11:27:08 +0100

    update login spacing

--- app/(views)/(auth)/components/auth-page.tsx
+++ app/(views)/(auth)/components/auth-page.tsx
@@ -7,10 +7,10 @@ interface AuthenticationProps {
 
 const AuthPage: React.FC<AuthenticationProps> = ({ heading, paragraph, formComponent, children }) => {
   return (
-    <div className="mx-auto mt-16 flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    <div className="mx-auto flex w-full flex-col justify-center space-y-6 pb-32 pt-8 sm:w-[350px]">
       <div className="flex flex-col space-y-2 text-center">
         <h1 className="text-2xl font-semibold tracking-tight">{heading}</h1>
-        <p className="text-sm text-muted-foreground">{paragraph}</p>
+        {/* <p className="text-sm text-muted-foreground">{paragraph}</p> */}
       </div>
       {formComponent || children}
     </div>
