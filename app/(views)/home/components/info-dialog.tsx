57c8f5ccab612d602963a918272e7940a4ec2d41
adon94 <adambfad@gmail.com>
2024-05-10 15:53:32 +0100

    update padding

--- app/(views)/home/components/info-dialog.tsx
+++ app/(views)/home/components/info-dialog.tsx
@@ -8,7 +8,7 @@ import ReactMarkdown from "react-markdown";
 export default function InfoDialog({ infoContent, closeDialog }) {
   return (
     <DialogContent className="scroll-fade-bottom my-16 max-h-[80vh] w-full max-w-xs overflow-scroll rounded-md px-4 py-0 md:max-w-2xl">
-      <ReactMarkdown className="prose prose-sm prose-indigo mx-auto my-4 w-full max-w-full px-4 py-8 leading-relaxed text-foreground">
+      <ReactMarkdown className="prose prose-sm prose-indigo mx-auto my-4 w-full max-w-full py-8 leading-relaxed text-foreground md:px-4">
         {infoContent}
       </ReactMarkdown>
       <div className="flex justify-center">
