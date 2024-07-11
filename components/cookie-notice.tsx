3b4d4a675860dff69e4f261c32c0720a03b8ceb3
adon94 <adambfad@gmail.com>
2024-07-11 17:52:45 +0100

    update cookie notice

--- components/cookie-notice.tsx
+++ components/cookie-notice.tsx
@@ -7,7 +7,7 @@ function CookieNotice() {
   return (
     <CookieConsent
       disableStyles
-      containerClasses="w-full bg-background fixed flex flex-col md:flex-row justify-between space-y-2 md:items-center z-50 p-4 border-t text-center md:text-start"
+      containerClasses="w-full bg-white/50 dark:bg-black/50 backdrop-blur-md fixed flex flex-col md:flex-row justify-between space-y-2 md:items-center z-50 p-4 border-t text-center md:text-start"
       contentClasses="flex items-center"
       buttonClasses={buttonVariants()}
     >
