8a6140ba4d6596c200e0a1e29d38b1fa062585c6
adon94 <adambfad@gmail.com>
2024-08-13 14:15:02 +0100

    update images next config to remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -5,21 +5,40 @@ import { env } from "./env.mjs";
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
-  reactStrictMode: false,
-  images: {
-    domains: ["ekxb0zdvbkmwks1n.public.blob.vercel-storage.com"],
-  },
-  experimental: { instrumentationHook: true, missingSuspenseWithCSRBailout: false },
-  output: "standalone",
-  rewrites() {
-    return [
-      { source: "/healthz", destination: "/api/health" },
-      { source: "/api/healthz", destination: "/api/health" },
-      { source: "/health", destination: "/api/health" },
-      { source: "/ping", destination: "/api/health" },
-    ];
-  },
-});
+const config = withPlugins(
+  [
+    [
+      withPWA({
+        dest: "public", // destination directory for the PWA files
+        disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+        register: true, // register the PWA service worker
+        skipWaiting: true, // skip waiting for service worker activation
+      }),
+      withBundleAnalyzer({ enabled: env.ANALYZE }),
+    ],
+  ],
+  {
+    reactStrictMode: false,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "ekxb0zdvbkmwks1n.public.blob.vercel-storage.com",
+          port: "",
+        },
+      ],
+    },
+    experimental: { instrumentationHook: true, missingSuspenseWithCSRBailout: false },
+    output: "standalone",
+    rewrites() {
+      return [
+        { source: "/healthz", destination: "/api/health" },
+        { source: "/api/healthz", destination: "/api/health" },
+        { source: "/health", destination: "/api/health" },
+        { source: "/ping", destination: "/api/health" },
+      ];
+    },
+  }
+);
 
 export default config;
