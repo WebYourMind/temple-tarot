e5c259dc405e2a2c7daa6c67b7db5ca74daef8fb
adon94 <adambfad@gmail.com>
2024-08-16 19:28:34 +0100

    logo

--- components/navigation/logo-component.tsx
+++ components/navigation/logo-component.tsx
@@ -1,17 +1,17 @@
 "use client";
 import Image from "next/image";
-// import Logo from "app/logo.png";
-import Logo2 from "app/android-chrome-192x192.png";
-import appConfig from "app.config";
-import { cn } from "lib/utils";
-import { MagicFont } from "components/tarot-session/query/query-input";
+import Logo from "app/logo.png";
+// import Logo2 from "app/android-chrome-192x192.png";
+// import appConfig from "app.config";
+// import { cn } from "lib/utils";
+// import { MagicFont } from "components/tarot-session/query/query-input";
 
 export default function LogoComponent() {
-  return (
-    <div className="flex w-full items-center justify-start md:px-2">
-      <Image src={Logo2} width={40} height={40} alt="Temple Tarot" className="mr-1 h-8 w-8 rounded-full shadow-sm" />
-      <h1 className={cn("my-4 text-xl", MagicFont.className)}>{appConfig.appName}</h1>
-    </div>
-  );
-  // return <Image src={Logo} width={200} height={100} alt="Temple Tarot" />;
+  // return (
+  //   <div className="flex w-full items-center justify-start md:px-2">
+  //     <Image src={Logo2} width={40} height={40} alt="Temple Tarot" className="mr-1 h-8 w-8 rounded-full shadow-sm" />
+  //     <h1 className={cn("my-4 text-xl", MagicFont.className)}>{appConfig.appName}</h1>
+  //   </div>
+  // );
+  return <Image src={Logo} width={200} height={100} alt="Temple Tarot" />;
 }
